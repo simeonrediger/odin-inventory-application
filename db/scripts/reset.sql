22f4e8c8c9f583271a907d23ebd5c8678579d1ae
Simeon Rediger <simeonrediger@gmail.com>
2026-06-22 19:24:03 -0400

    Add genre_artists join table

--- db/scripts/reset.sql
+++ db/scripts/reset.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS genre_artists;
 DROP TABLE IF EXISTS genres;
 DROP TABLE IF EXISTS artists;
 DROP DOMAIN IF EXISTS trimmed_nonblank_text;
@@ -15,4 +16,10 @@ CREATE TABLE genres (
 CREATE TABLE artists (
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name trimmed_nonblank_text NOT NULL UNIQUE
+);
+
+CREATE TABLE genre_artists (
+  genre_id integer NOT NULL REFERENCES genres,
+  artist_id integer NOT NULL REFERENCES artists,
+  PRIMARY KEY (genre_id, artist_id)
 );
