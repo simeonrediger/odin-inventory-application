f3c6a894def3634b7997310ec0be9d5ed06b4010
Simeon Rediger <simeonrediger@gmail.com>
2026-06-21 21:44:59 -0400

    Add base styles to site layout

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('views', path.join(import.meta.dirname, 'views/pages'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(import.meta.dirname, 'public')));
 app.use('/', homeRouter);
 
 const PORT = process.env.PORT || 3000;
