4e11352ed0baaf422d29d2f42f23d2d7e6af1313
Simeon Rediger <simeonrediger@gmail.com>
2026-06-22 21:21:52 -0400

    Remove newlines and reformat

--- db/genres.queries.js
+++ db/genres.queries.js
@@ -1,12 +1,6 @@
 import pool from './pool.js';
 
 export async function findGenres() {
-  const { rows } = await pool.query(
-    `
-    SELECT * FROM genres
-    ORDER BY name
-    `,
-  );
-
+  const { rows } = await pool.query('SELECT * FROM genres ORDER BY name');
   return rows;
 }
