ca36afe47c5f1b81f569cbcf537763390de356b6
Simeon Rediger <simeonrediger@gmail.com>
2026-06-24 16:16:35 -0400

    Add records

--- db/scripts/artists.js
+++ db/scripts/artists.js
@@ -2,62 +2,297 @@ const artists = [
   {
     name: 'A Perfect Circle',
     genres: ['alternative', 'metal', 'rock'],
+    records: [
+      {
+        name: 'eMOTIVe',
+        price: 899,
+        quantity: 4,
+      },
+      {
+        name: 'Mer de Noms',
+        price: 899,
+        quantity: 2,
+      },
+      {
+        name: 'Thirteenth Step',
+        price: 1999,
+        quantity: 3,
+      },
+    ],
   },
   {
     name: 'Carpenter Brut',
     genres: ['electronic'],
+    records: [
+      {
+        name: 'TRILOGY',
+        price: 2679,
+        quantity: 1,
+      },
+    ],
   },
   {
     name: 'Jimmy Eat World',
     genres: ['emo', 'pop', 'punk', 'rock'],
+    records: [
+      {
+        name: 'Bleed American',
+        price: 800,
+        quantity: 6,
+      },
+      {
+        name: 'Futures',
+        price: 499,
+        quantity: 3,
+      },
+    ],
   },
   {
     name: 'Nothing But Thieves',
     genres: ['alternative', 'pop', 'rock'],
+    records: [
+      {
+        name: 'Broken Machine',
+        price: 2118,
+        quantity: 3,
+      },
+      {
+        name: 'Nothing But Thieves',
+        price: 2198,
+        quantity: 3,
+      },
+    ],
   },
   {
     name: 'Queens of the Stone Age',
     genres: ['alternative', 'metal', 'rock'],
+    records: [
+      {
+        name: 'Era Vulgaris',
+        price: 1499,
+        quantity: 3,
+      },
+      {
+        name: 'Lullabies to Paralyze',
+        price: 800,
+        quantity: 1,
+      },
+      {
+        name: 'Songs for the Deaf',
+        price: 1500,
+        quantity: 7,
+      },
+    ],
   },
   {
     name: 'Radiohead',
     genres: ['alternative', 'indie', 'rock'],
+    records: [
+      {
+        name: 'In Rainbows',
+        price: 1057,
+        quantity: 13,
+      },
+      {
+        name: 'OK Computer',
+        price: 1900,
+        quantity: 7,
+      },
+      {
+        name: 'The Bends',
+        price: 1499,
+        quantity: 3,
+      },
+    ],
   },
   {
     name: 'RÜFÜS DU SOL',
     genres: ['dance', 'electronic'],
+    records: [
+      {
+        name: 'Bloom',
+        price: 4498,
+        quantity: 2,
+      },
+      {
+        name: 'SOLACE',
+        price: 1498,
+        quantity: 1,
+      },
+    ],
   },
   {
     name: 'So Below',
     genres: ['electronic', 'indie', 'pop'],
+    records: [
+      {
+        name: 'II',
+        price: 500,
+        quantity: 1,
+      },
+      {
+        name: 'Left Behind',
+        price: 999,
+        quantity: 2,
+      },
+    ],
   },
   {
     name: 'Spoon',
     genres: ['alternative', 'indie', 'rock'],
+    records: [
+      {
+        name: 'Gimme Fiction',
+        price: 300,
+        quantity: 4,
+      },
+      {
+        name: 'Hot Thoughts',
+        price: 844,
+        quantity: 3,
+      },
+      {
+        name: 'They Want My Soul',
+        price: 945,
+        quantity: 2,
+      },
+    ],
   },
   {
     name: 'System of a Down',
     genres: ['alternative', 'metal', 'rock'],
+    records: [
+      {
+        name: 'Hypnotize',
+        price: 1000,
+        quantity: 5,
+      },
+      {
+        name: 'Steal This Album!',
+        price: 1199,
+        quantity: 4,
+      },
+      {
+        name: 'Toxicity',
+        price: 749,
+        quantity: 11,
+      },
+    ],
   },
   {
     name: 'The Shins',
     genres: ['alternative', 'indie', 'pop', 'rock'],
+    records: [
+      {
+        name: 'Oh, Inverted World',
+        price: 500,
+        quantity: 3,
+      },
+      {
+        name: 'Wincing the Night Away',
+        price: 299,
+        quantity: 2,
+      },
+    ],
   },
   {
     name: 'The Strokes',
     genres: ['alternative', 'indie', 'rock'],
+    records: [
+      {
+        name: 'First Impressions of Earth',
+        price: 499,
+        quantity: 5,
+      },
+      {
+        name: 'Is This It',
+        price: 600,
+        quantity: 9,
+      },
+      {
+        name: 'Room on Fire',
+        price: 199,
+        quantity: 6,
+      },
+      {
+        name: 'The New Abnormal',
+        price: 1297,
+        quantity: 6,
+      },
+    ],
   },
   {
     name: 'Tool',
     genres: ['alternative', 'metal', 'rock'],
+    records: [
+      {
+        name: '10,000 Days',
+        price: 2500,
+        quantity: 18,
+      },
+      {
+        name: 'Ænima',
+        price: 1299,
+        quantity: 9,
+      },
+      {
+        name: 'Lateralus',
+        price: 1250,
+        quantity: 12,
+      },
+      {
+        name: 'Undertow',
+        price: 1000,
+        quantity: 6,
+      },
+    ],
   },
   {
     name: 'Two Door Cinema Club',
     genres: ['alternative', 'indie', 'rock'],
+    records: [
+      {
+        name: 'Beacon',
+        price: 1310,
+        quantity: 5,
+      },
+      {
+        name: 'Gameshow',
+        price: 960,
+        quantity: 2,
+      },
+      {
+        name: 'Tourist History',
+        price: 618,
+        quantity: 7,
+      },
+    ],
   },
   {
     name: 'White Lies',
     genres: ['alternative', 'dance', 'indie', 'rock'],
+    records: [
+      {
+        name: 'BIG TV',
+        price: 1400,
+        quantity: 2,
+      },
+      {
+        name: 'Friends',
+        price: 1073,
+        quantity: 5,
+      },
+      {
+        name: 'FIVE',
+        price: 2072,
+        quantity: 2,
+      },
+      {
+        name: 'To Lose My Life...',
+        price: 498,
+        quantity: 3,
+      },
+    ],
   },
 ];
 
--- db/scripts/seed.js
+++ db/scripts/seed.js
@@ -5,6 +5,7 @@ export default async function seed(client) {
   await seedGenres(client);
   await seedArtists(client);
   await seedGenreArtists(client);
+  await seedRecords(client);
 }
 
 async function seedGenres(client) {
@@ -37,6 +38,21 @@ async function seedGenreArtists(client) {
       AS seed_artists(name text, genres text[])
     INNER JOIN genres ON genres.name = ANY(seed_artists.genres)
     INNER JOIN artists ON artists.name = seed_artists.name
+    `,
+    [JSON.stringify(artists)],
+  );
+}
+
+async function seedRecords(client) {
+  await client.query(
+    `
+    INSERT INTO records (name, artist_id, quantity, price)
+    SELECT record.name, artist.id, record.quantity, record.price
+    FROM json_to_recordset($1::json)
+      AS seed_artist(name text, records json)
+    INNER JOIN artists AS artist ON artist.name = seed_artist.name
+    CROSS JOIN LATERAL json_to_recordset(seed_artist.records)
+      AS record(name text, quantity integer, price bigint)
     `,
     [JSON.stringify(artists)],
   );
