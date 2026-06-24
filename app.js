1a411c0fc0cf7e65db8a3bb67df0eaafc731bb94
Simeon Rediger <simeonrediger@gmail.com>
2026-06-24 18:34:27 -0400

    Rearrange imports and routes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import path from 'node:path';
 import express from 'express';
 
 import homeRouter from './routes/home.router.js';
-import artistsRouter from './routes/artists.router.js';
 import genresRouter from './routes/genres.router.js';
+import artistsRouter from './routes/artists.router.js';
 
 const app = express();
 app.set('views', path.join(import.meta.dirname, 'views/pages'));
@@ -11,8 +11,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(import.meta.dirname, 'public')));
 app.use('/', homeRouter);
-app.use('/artists', artistsRouter);
 app.use('/genres', genresRouter);
+app.use('/artists', artistsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, error => {
