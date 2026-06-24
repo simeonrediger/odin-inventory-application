18adb29c3f238d4b671e4c51451f66f26bbc4342
Simeon Rediger <simeonrediger@gmail.com>
2026-06-24 19:09:20 -0400

    Add more genres

--- db/scripts/genres.js
+++ db/scripts/genres.js
@@ -6,13 +6,17 @@ const genres = [
   'dance',
   'electronic',
   'emo',
+  'folk',
   'hip-hop',
   'indie',
   'jazz',
+  'lo-fi',
   'metal',
   'pop',
   'punk',
+  'reggae',
   'rock',
+  'world music',
 ];
 
 export default genres;
