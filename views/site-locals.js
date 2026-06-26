20c522d311a9c8b8490e404412e982196d288abf
Simeon Rediger <simeonrediger@gmail.com>
2026-06-26 17:43:07 -0400

    Extract path construction to siteLocals functions

--- views/site-locals.js
+++ views/site-locals.js
@@ -5,10 +5,33 @@ export function assign(req, res, next) {
 
 const siteLocals = {
   siteName: 'Record Store',
-  getSlug(text) {
-    return text.toLowerCase().replaceAll(' ', '-');
-  },
+  getSlug,
   formatPrice(price) {
     return '$' + Number(price / 100).toFixed(2);
   },
+  getHomePath() {
+    return '/';
+  },
+  getGenresPath() {
+    return '/genres';
+  },
+  getGenrePath({ id, name }) {
+    return `/genres/${id}/${getSlug(name)}`;
+  },
+  getArtistsPath() {
+    return '/artists';
+  },
+  getArtistPath({ id, name }) {
+    return `/artists/${id}/${getSlug(name)}`;
+  },
+  getRecordsPath() {
+    return '/records';
+  },
+  getRecordPath({ id, name }) {
+    return `/records/${id}/${getSlug(name)}`;
+  },
 };
+
+function getSlug(text) {
+  return text.toLowerCase().replaceAll(' ', '-');
+}
