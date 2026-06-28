fac9a4f1c3d968714472908f10fca0ab159ea6ea
Simeon Rediger <simeonrediger@gmail.com>
2026-06-27 23:49:45 -0400

    Remove artist records page

--- controllers/artists.controller.js
+++ controllers/artists.controller.js
@@ -4,9 +4,3 @@ export async function getArtists(req, res) {
   const artists = await db.artists.find();
   res.render('artists', { pageName: 'Artists', artists });
 }
-
-export async function getArtistRecords(req, res) {
-  const { id } = req.params;
-  const artist = await db.artists.findByIdWithRecords(id);
-  res.render('artist-records', { pageName: artist.name, artist });
-}
--- routes/artists.router.js
+++ routes/artists.router.js
@@ -4,6 +4,5 @@ import * as artistsController from '../controllers/artists.controller.js';
 const artistsRouter = Router();
 
 artistsRouter.get('/', artistsController.getArtists);
-artistsRouter.get('/:id/:slug', artistsController.getArtistRecords);
 
 export default artistsRouter;
