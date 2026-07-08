d061ed3ed8692054f686a8e6f30788dbe5f3a93e
Simeon Rediger <simeonrediger@gmail.com>
2026-07-08 19:03:54 -0400

    Add artists table with name, genres, and records

--- controllers/artists.controller.js
+++ controllers/artists.controller.js
@@ -1,6 +1,16 @@
 import db from '../db/queries.js';
 
 export async function getArtists(req, res) {
-  const artists = await db.artists.find();
-  res.render('artists', { pageName: 'Artists', artists });
+  const { artists, genres } = await getPageData(req);
+  res.render('artists', { pageName: 'Artists', artists, genres });
+}
+
+async function getPageData(req) {
+  const artists = await db.artists.find({
+    includeGenres: true,
+    includeRecords: true,
+  });
+
+  const genres = await db.genres.find();
+  return { artists, genres };
 }
--- db/artists.queries.js
+++ db/artists.queries.js
@@ -1,13 +1,52 @@
 import pool from './pool.js';
 
-export async function find() {
-  const { rows } = await pool.query(
-    `
-    SELECT * FROM artists
-    ORDER BY name
-    `,
-  );
+export async function find({
+  includeGenres = false,
+  includeRecords = false,
+} = {}) {
+  let sql = 'SELECT artists.*';
 
+  if (includeGenres) {
+    sql += ', g.genres';
+  }
+
+  if (includeRecords) {
+    sql += ', r.records';
+  }
+
+  sql += ' FROM artists';
+
+  if (includeGenres) {
+    sql += `
+      LEFT JOIN LATERAL (
+        SELECT COALESCE(
+          jsonb_agg(to_jsonb(genres) ORDER BY genres.name),
+          '[]'::jsonb
+        ) AS genres
+        FROM genre_artists
+        INNER JOIN genres
+          ON genres.id = genre_artists.genre_id
+        WHERE genre_artists.artist_id = artists.id
+      ) AS g ON TRUE
+    `;
+  }
+
+  if (includeRecords) {
+    sql += `
+      LEFT JOIN LATERAL (
+        SELECT COALESCE(
+          jsonb_agg(to_jsonb(records) ORDER BY records.name),
+          '[]'::jsonb
+        ) AS records
+        FROM records
+        WHERE records.artist_id = artists.id
+      ) AS r ON TRUE
+    `;
+  }
+
+  sql += ' ORDER BY artists.name';
+
+  const { rows } = await pool.query(sql);
   return rows;
 }
 
